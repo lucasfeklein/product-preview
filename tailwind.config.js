04f501792f987e612dbb82ef5099e5a2081bb17b
lucasfeklein <lucas.fklein@hotmail.com>
2023-10-29 15:50:57 +0800

    unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,11 +20,3 @@ export default {
   },
   plugins: [],
 };
-
-// - Dark cyan: hsl(158, 36%, 37%)
-// - Cream: hsl(30, 38%, 92%)
-
-// ### Neutral
-
-// - Very dark blue: hsl(212, 21%, 14%)
-// - Dark grayish blue: hsl(228, 12%, 48%)
